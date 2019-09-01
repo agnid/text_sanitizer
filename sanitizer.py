3ffdb1b03fe4d06f392b123a38e9eec5ad80f142
agnid <53524203+agnid@users.noreply.github.com>
2019-09-01 16:26:53 +0200

    Rm io lib

--- sanitizer.py
+++ sanitizer.py
@@ -1,17 +1,16 @@
 import unidecode
 import sys
 import os
-import io
 
 '''
-TBD: check for newline chars
+TBD: better support for non-latin languages 
 '''
 
 if "--input" in sys.argv:
     input_path = str(sys.argv[sys.argv.index("--input") + 1])
     while os.path.isfile(input_path) is False:
         input_path = str(input("Provide path to correct data source (must be a file): "))
-    text = io.open(input_path, "r", encoding="utf-8").read()
+    text = open(input_path, "rt", encoding="utf-8").read()
 else:
     text = str(input("Your text: "))
 
@@ -25,6 +24,7 @@ def sanitizer(source):
     return unidecode.unidecode(source)
 
 
-output_file = io.open(output_path, "w", encoding="utf-8")
+output_file = open(output_path, "w", encoding="utf-8")
 output_file.write(sanitizer(text))
 output_file.close()
+print(f"Parsed text was successfully saved in {output_path}")
